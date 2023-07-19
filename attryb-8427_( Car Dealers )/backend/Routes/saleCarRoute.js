c923e6537fe36bccb37ece387634000bb5b7e889
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-07-19 14:24:16 +0530

    userModal and controler has been done

--- attryb-8427_( Car Dealers )/backend/Routes/saleCarRoute.js
+++ attryb-8427_( Car Dealers )/backend/Routes/saleCarRoute.js
@@ -3,10 +3,11 @@
 let mongoose = require("mongoose");
 let express = require("express");
 const { dealerAuth } = require("../Middleware/dealerAuth");
+const { userAuth } = require("../Middleware/userAuth");
 
 let saleCarRoute = express.Router();
 
-saleCarRoute.get("/", dealerAuth, async (req, res) => {
+saleCarRoute.get("/", userAuth, async (req, res) => {
   res.send("sale car route");
 });
 
--- attryb-8427_( Car Dealers )/backend/Routes/userRoute.js
+++ attryb-8427_( Car Dealers )/backend/Routes/userRoute.js
@@ -1,12 +1,22 @@
 /** @format */
-
-let mongoose = require("mongoose");
 let express = require("express");
+
+const bcrypt = require("bcrypt");
+const {
+  createUser,
+  getUser,
+  loginUser,
+} = require("../controller/userController");
 
 let userRoute = express.Router();
 
-userRoute.get("/", async (req, res) => {
-  res.send("userPage");
-});
+// API for create user
+userRoute.post("/create", createUser);
+
+// API for get all user
+userRoute.get("/", getUser);
+
+// APi for login user
+userRoute.post("/login", loginUser);
 
 module.exports = { userRoute };
